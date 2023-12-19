58a9b41c00e8cdb57b2c368325f9fd81c51ce5e5
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-19 03:07:19 -0600

    Add setup scripts and stations.csv

---
+++ sh_repo_download.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/briansgithub/TideTracker_repo.git
+
+# Make the script executable with the following command:
+# chmod +x thisScriptName.sh
---
+++ sh_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash 
+
+# Shebang taken from the wifi portal setup
+# Make this script executable with the following command:
+# chmod +x run_command.sh
+
+# Run the script with:
+# ./run_command.sh
