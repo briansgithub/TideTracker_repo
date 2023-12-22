c424a39fbb282b7a835ec126853cd1188933248e
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-22 04:39:04 -0600

    Run NOAA data plot and screen refresh via shell script

--- sh_epaperDemoRun.sh
+++
@@ -1,8 +0,0 @@
-
-# Uncomment below if you need to download the e-paper demo repo
-# git clone https://github.com/waveshareteam/e-Paper.git
-
-python3 ./e-Paper/RaspberryPi_JetsonNano/python/examples/epd_7in5_V2_test.py
-
-# Make the script executable with the following command:
-# chmod +x thisScriptName.sh
---
+++ sh_run_TideTracker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash 
+
+python3 pull_json_and_plot.py
+python3 update_epd_7in5_V2_screen.py
