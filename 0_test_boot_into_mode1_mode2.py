1de9ee2a1d1408bea3b86d44fa842360540e7a3d
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-23 00:14:41 -0600

    Working GPIO sense script

--- 0_test_boot_into_mode1_mode2.py
+++ 0_test_boot_into_mode1_mode2.py
@@ -9,25 +9,9 @@ gpio_pin = 16  # Replace with your GPIO pin number
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Function to execute commands based on GPIO state
-def execute_commands():
-    if GPIO.input(gpio_pin) == GPIO.HIGH:
-        # Command to execute when GPIO is HIGH
-        # subprocess.run(["your_high_command_here"])
-        print("RESULT: GPIO is HIGH")
-    else:
-        # Command to execute when GPIO is LOW
-        # subprocess.run(["your_low_command_here"])
-        print("RESULT: GPIO is LOW")
+if GPIO.input(gpio_pin) == GPIO.HIGH:
+    subprocess.run(["your_high_command_here"])
+else:
+        subprocess.run(["your_low_command_here"])
 
-# Main loop
-try:
-    while True:
-        execute_commands()
-        time.sleep(1)  # Adjust the sleep duration as needed
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    GPIO.cleanup()
+GPIO.cleanup()
