f8692e179a46fd35d659152286d6f9dec074d2a5
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-23 00:08:47 -0600

    Fix GPIO pin number to match BCM pin number. .sh-->.py

---
+++ 0_test_boot_into_mode1_mode2 copy.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+# Define the GPIO pin you want to monitor
+gpio_pin = 16  # Replace with your GPIO pin number
+
+# Set up GPIO mode and pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Function to execute commands based on GPIO state
+def execute_commands():
+    if GPIO.input(gpio_pin) == GPIO.HIGH:
+        # Command to execute when GPIO is HIGH
+        subprocess.run(["your_high_command_here"])
+    else:
+        # Command to execute when GPIO is LOW
+        subprocess.run(["your_low_command_here"])
+
+# Main loop
+try:
+    while True:
+        execute_commands()
+        time.sleep(1)  # Adjust the sleep duration as needed
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    GPIO.cleanup()
