166912898f5a23380d229fd9d53e7d5004b10142
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-23 13:48:31 -0600

    Refactor var names

--- 0_boot_sense.py
+++ 0_boot_sense.py
@@ -10,16 +10,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Define script names and their paths
-pi_portal_sh = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'run.sh')
-get_data_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), '1_pull_json_and_plot.py')
-update_screen = os.path.join(os.path.dirname(os.path.realpath(__file__)), '2_update_epd_7in5_V2_screen.py')
+pi_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'run.sh')
+get_data_script_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '1_pull_json_and_plot.py')
+update_screen_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '2_update_epd_7in5_V2_screen.py')
 
 try:
     if GPIO.input(gpio_pin) == GPIO.HIGH:
-        subprocess.run(['sudo', 'bash', pi_portal_sh])
+        subprocess.run(['sudo', 'bash', pi_wifi_path])
     else:
-        subprocess.run(['python3', get_data_script])
-        subprocess.run(['python3', update_screen])
+        subprocess.run(['python3', get_data_script_path])
+        subprocess.run(['python3', update_screen_path])
 
 finally:
     # Cleanup GPIO settings
