4971c5e92cde7f5ba2893b50950f15890f33819a
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-24 10:30:24 -0600

    Test some different code to make error colors red

--- a.sh
+++ a.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
-# A quick shell script for running the commands I need to run often
+# A quick shell script for running the commands I need to run so often
 set -x
+
+# Function to print colored text
+print_error() {
+    tput setaf 1;  # Set text color to red
+    echo "ERROR: $1";
+    tput sgr0;  # Reset text color
+}
 
 # Reset Git repository
 git reset --hard
 if [ $? -ne 0 ]; then
-    echo -e "\e[31mERROR: GIT RESET FAILED\e[0m" | tr '[:lower:]' '[:upper:]'
-    # Reset permissions on scripts
-    sudo chmod +x /home/pi/TideTracker_repo/*.sh
-    sudo chmod +x /home/pi/TideTracker_repo/forked_wifi-connect-headless-rpi/scripts/*.sh
+    print_error "Git reset failed"
     exit 1
 fi
-
-
 
 # Pull from Git
 git pull
 if [ $? -ne 0 ]; then
-    echo -e "\e[31mERROR: GIT PULL FAILED\e[0m" | tr '[:lower:]' '[:upper:]'
+    print_error "Git pull failed"
     exit 1
 fi
 
