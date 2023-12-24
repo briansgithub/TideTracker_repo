9d62af550f1b38c91d5dda81187569c9653185e3
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-24 09:37:14 -0600

    Disable 0_boot_sense.py in convenience script, for now

--- a.sh
+++ a.sh
@@ -5,4 +5,4 @@ git reset --hard
 git pull
 sudo chmod +x /home/pi/TideTracker_repo/*.sh
 sudo chmod +x /home/pi/TideTracker_repo/forked_wifi-connect-headless-rpi/scripts/*.sh
-python3 0_boot_sense.py
+#python3 0_boot_sense.py
