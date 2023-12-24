833ada12ed6e917ac84ee96daf2e5db04af7ae4d
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-24 10:57:02 -0600

    Undo the convenience of chmod right after Git reset (it interferes with git pull)

--- a.sh
+++ a.sh
@@ -14,13 +14,8 @@ git reset --hard
 if [ $? -ne 0 ]; then
     print_error "GIT RESET FAILED!"
     # Reset permissions on scripts
-    sudo chmod +x /home/pi/TideTracker_repo/*.sh
-    sudo chmod +x /home/pi/TideTracker_repo/forked_wifi-connect-headless-rpi/scripts/*.sh
     exit 1
 fi
-
-sudo chmod +x /home/pi/TideTracker_repo/*.sh
-sudo chmod +x /home/pi/TideTracker_repo/forked_wifi-connect-headless-rpi/scripts/*.sh
 
 # Pull from Git
 git pull
