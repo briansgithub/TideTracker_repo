4d246e95b284220d185764a2b05a759bd30d7832
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-25 13:47:36 -0600

    Try combining both fetch data and plot graph into one script

--- 0_boot_sense.py
+++ 0_boot_sense.py
@@ -12,21 +12,19 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Define script names and their paths
-pi_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'run.sh')
-NOAA_pull_script_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '1_pull_json_and_plot.py')
-refresh_eInk_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '2_update_epd_7in5_V2_screen.py')
+auto_run_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'run.sh')
+delete_and_change_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'del-run.sh')
+plot_tides_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '1_pull_json_and_plot.py')
 
 try:
     if GPIO.input(gpio_pin) == GPIO.HIGH:
         command = "sudo systemctl start NetworkManager"
         subprocess.run(command, shell=True, check=True)
         time.sleep(15)
-        # pi_wifi_path = pi_wifi_path.replace('run.sh', 'del-run.sh') # to delete wifi connections
-        subprocess.run(['sudo', 'bash', pi_wifi_path], check=True)
+        subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
     else:
-        time.sleep(15)
-        subprocess.run(['python3', NOAA_pull_script_path], check=True)
-        subprocess.run(['python3', refresh_eInk_path], check=True)
+        subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
+        subprocess.run(['python3', plot_tides_path], check=True)
 
 finally:
     # Cleanup GPIO settings
--- 1_pull_json_and_plot.py
+++ 1_pull_json_and_plot.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'e-ink_lib')
+maindir = os.path.dirname(os.path.realpath(__file__))
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime as dt
@@ -14,8 +30,7 @@ import numpy as np
 
 
 print("BEGINNING")
-DISPLAY_PLOT = False
-# DISPLAY_PLOT = True
+DISPLAY_PLOT = True
 
 YEXTEND = 0.175 # y-axis addition to move labels and extend ylim0 and ylim1 to make room for labels and present data
 
@@ -333,3 +348,35 @@ if __name__ == "__main__":
     data_json = fetch_NOAA_data(station_id, yesterday_d)
 
     plot_data(data_json, now_dtz)
+
+
+    try:
+        epd = epd7in5_V2.EPD()
+        ### logging.info("\ninit and Clear\n")
+        epd.init()
+        ### epd.Clear()
+
+        logging.info("\nDisplaying the .bmp on the e-ink display)\n")
+        plot_image = Image.open(os.path.join(maindir, 'plot_image.bmp'))
+        plot_image = plot_image.transpose(Image.ROTATE_180)
+        epd.display(epd.getbuffer(plot_image))
+        #time.sleep(2)
+
+        ### # Initialize a canvas. Open a file and display it on the canvas. 
+        ### logging.info("4. Create composite images")
+        ### Himage2 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+        ### bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+        ### Himage2.paste(bmp, (50,10))
+        ### epd.display(epd.getbuffer(Himage2))
+        ### time.sleep(2)
+
+        logging.info("\nGoto Sleep...\n")
+        epd.sleep()
+        
+    except IOError as e:
+        logging.info(e)
+        
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd7in5_V2.epdconfig.module_exit()
+        exit()
