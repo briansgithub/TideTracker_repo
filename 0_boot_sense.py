793783374cf273d28cb9bc0a58842a507bdc48e1
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-25 16:16:45 -0600

    Revert "Try squashing everything into one file"
    This reverts commit 0a60afae58e1a085d2a6d2c09cbcd80366572496.

--- 0_boot_sense.py
+++ 0_boot_sense.py
@@ -16,23 +16,23 @@ auto_run_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '
 # delete_and_change_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'del-run.sh')
 # NEVER use -d to delete the wifi!? The wifi portal code seems to never autoconnect to wifi normally again after that
 
-plot_tides_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '1_pull_json_and_plot.py')
+    tides_exec_script_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_tides.sh')
 
-try:
-    pin_state = GPIO.input(gpio_pin)
-    print(f"GPIO Pin BMC# {gpio_pin} is {pin_state}")
-    if pin_state == GPIO.HIGH:
-        
-        command = "sudo systemctl start NetworkManager"
-        subprocess.run(command, shell=True, check=True)
-        time.sleep(15)
-        subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
-    else:
-        print(f"\nRunning the wifi script located at:\n\t{auto_run_wifi_path}\n\t")
-        subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
+    try:
+        pin_state = GPIO.input(gpio_pin)
+        print(f"\n\nGPIO Pin BCM# {gpio_pin} is {pin_state}\n")
+        if pin_state == GPIO.HIGH:
+            
+            command = "sudo systemctl start NetworkManager"
+            subprocess.run(command, shell=True, check=True)
+            time.sleep(15)
+            subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
+        else:
+            time.sleep(15)
+            print(f"-------- Running the wifi script located at:\n\t{auto_run_wifi_path} ---------")
+            subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
 
-        print(f"\nRunning the tides script located at:\n\t{plot_tides_path}\n\t")
-        subprocess.run(['sudo', 'python3', plot_tides_path], check=True)
+            main()
 
 finally:
     # Cleanup GPIO settings
