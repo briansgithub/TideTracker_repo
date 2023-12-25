3a3efddd27d41b1b3231add66087e5e491d9e909
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-25 16:01:19 -0600

    Revert "Add sleep steps to the boot sense launcher"
    This reverts commit c9a07fbfd401fd9785d810024adcac152e06a543.

--- 0_boot_sense.py
+++ 0_boot_sense.py
@@ -4,57 +4,34 @@ import subprocess
 import time
 import requests
 
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-import sys
-import os
-libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'e-ink_lib')
-maindir = os.path.dirname(os.path.realpath(__file__))
-if os.path.exists(libdir):
-    sys.path.append(libdir)
+# Define the GPIO pin you want to monitor
+gpio_pin = 16  # Replace with your GPIO pin number
 
-import logging
-from waveshare_epd import epd7in5_V2
-import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
+# Set up GPIO mode and pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+# Define script names and their paths
+auto_run_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'run.sh')
+# delete_and_change_wifi_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'forked_wifi-connect-headless-rpi', 'scripts', 'del-run.sh')
+# NEVER use -d to delete the wifi!? The wifi portal code seems to never autoconnect to wifi normally again after that
 
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import datetime as dt
-import requests
-from PIL import Image
-import csv
-import pytz
-from timezonefinder import TimezoneFinder
-import ephem
-from matplotlib.ticker import FuncFormatter
-import os
-from scipy.signal import find_peaks
-import numpy as np
+plot_tides_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '1_pull_json_and_plot.py')
 
-
-print("BEGINNING")
-DISPLAY_PLOT = True
-
-YEXTEND = 0.175 # y-axis addition to move labels and extend ylim0 and ylim1 to make room for labels and present data
-
-PERIOD = 2 #hours between TPL5110 reloads
-STATIC_TIMEZONE = True #used to set timezone to Fort Myers so get_timezone is averted
-# The data and axes will always be in local time, 
-# but the sunrise/sunset and the present run of data
-# are affected by the timezoneyou apply
-
-def get_timezone(latitude, longitude):
-    if(STATIC_TIMEZONE):
-        return pytz.timezone('US/Eastern') #Fort Myers
-    else:
+try:
+    pin_state = GPIO.input(gpio_pin)
+    print(f"GPIO Pin BMC# {gpio_pin} is {pin_state}")
+    if pin_state == GPIO.HIGH:
+        
+        command = "sudo systemctl start NetworkManager"
+        subprocess.run(command, shell=True, check=True)
         time.sleep(15)
-        print(f"-------- Running the wifi script located at:\n\t{auto_run_wifi_path} ---------")
         subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
-        time.sleep(1)
-        print(f"--------- \nRunning the tides script located at:\n\t{plot_tides_path} ---------")
+    else:
+        print(f"\nRunning the wifi script located at:\n\t{auto_run_wifi_path}\n\t")
+        subprocess.run(['sudo', 'bash', auto_run_wifi_path], check=True)
+
+        print(f"\nRunning the tides script located at:\n\t{plot_tides_path}\n\t")
         subprocess.run(['sudo', 'python3', plot_tides_path], check=True)
 
     finally:
