93c0990c354511f3351c51bbc20d3ca189dc2a4c
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-25 17:50:21 -0600

    Update test to fix image size to be epd screen size

--- test_no_wifi_paste_over.py
+++ test_no_wifi_paste_over.py
@@ -21,13 +21,15 @@ logging.info("epd7in5_V2 Paste over")
 epd = epd7in5_V2.EPD()
 epd.init()
 
+Himage2 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+
 plot_image = Image.open(os.path.join(maindir, 'plot_image.bmp'))
 plot_image = plot_image.transpose(Image.ROTATE_180)
 
 logging.info("Paste error message over screen window")
 
 err_img = Image.open(os.path.join(maindir, 'no_wifi.bmp'))
-err_img.paste(err_img)
+err_img = plot_image.paste(err_img)
 epd.display(epd.getbuffer(err_img))
 
 logging.info("EPD Go to Sleep...")
