08377427c99e57870b8772103d82d740053478bc
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-25 17:13:03 -0600

    Fix the imports in past-over test

--- test_no_wifi_paste_over.py
+++ test_no_wifi_paste_over.py
@@ -3,10 +3,11 @@
 
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'e-ink_lib')
+maindir = os.path.dirname(os.path.realpath(__file__))
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
