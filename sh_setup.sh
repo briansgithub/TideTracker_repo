b7f5fc627466eeeedb9f760ef03da71dff37b3ec
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-25 16:04:37 -0600

    Remove boot service setup from sh_setup.sh Switching to using cron

--- sh_setup.sh
+++ sh_setup.sh
@@ -129,48 +129,9 @@ echo -e "\n##### END SETUP: NOAA PULL AND PLOT LIBS #####\n"
 
 
 echo -e "\n##### SETUP: SET UP SCRIPT TO RUN ON BOOT #####\n" 
-# Set the username automatically
-# USERNAME=$(whoami)
 
-# Define paths and filenames
-SERVICE_FILE_CONTENT=$(cat <<EOL
-# /etc/systemd/system/0_boot_sense.service
+# boot service removed. Changing to cron 
 
-[Unit]
-Description=Boot Sense
-After=network.target
-After=final.target
-
-[Service]
-ExecStart=/usr/bin/python3 /home/pi/TideTracker_repo/0_boot_sense.py
-Restart=no
-
-[Install]
-WantedBy=multi-user.target
-
-EOL
-)
-
-### After=multi-user.target
-### After=network-online.target
-
-SERVICE_FILENAME="0_boot_sense.service"
-SYSTEMD_PATH="/etc/systemd/system/"
-
-# Write the service file content to a temporary file
-echo "$SERVICE_FILE_CONTENT" > "$SERVICE_FILENAME"
-
-# Move the service file to /etc/systemd/system/
-sudo mv "$SERVICE_FILENAME" "$SYSTEMD_PATH"
-
-# Reload systemd to apply changes
-sudo systemctl daemon-reload
-
-# Enable and start the service
-sudo systemctl enable "$SERVICE_FILENAME"
-sudo systemctl start "$SERVICE_FILENAME"
-
-echo "Boot service setup complete."
 echo -e "\n##### END SETUP: SET UP SCRIPT TO RUN ON BOOT #####\n" 
 
 
