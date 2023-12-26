302c1c24f96c2e8978920ffc32723c423367cd46
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-26 07:14:38 -0600

    Edit uptime test

--- deleteme_uptime_tests.py
+++ deleteme_uptime_tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import psutil
-from datetime import timedelta
+from datetime import timedelta, datetime
 import json
 
 def get_uptime():
@@ -12,17 +12,41 @@ def get_uptime():
 
     return uptime_timedelta
 
-def save_to_json(uptime, filename="uptime.json"):
-    data = {"uptime_seconds": uptime.total_seconds(), "uptime_formatted": str(uptime)}
+def get_current_time():
+    # Get the current time
+    current_time = datetime.now()
+    
+    return current_time
+
+def get_time_difference(uptime, current_time):
+    # Calculate the time difference between now and uptime
+    time_difference = current_time - uptime
+
+    return time_difference
+
+def save_to_json(uptime, current_time, time_difference, filename="uptime.json"):
+    data = {
+        "uptime_seconds": uptime.total_seconds(),
+        "uptime_formatted": str(uptime),
+        "current_time": str(current_time),
+        "time_difference": str(time_difference)
+    }
     with open(filename, "w") as json_file:
         json.dump(data, json_file, indent=4)
 
 def main():
     uptime = get_uptime()
+    current_time = get_current_time()
+
+    # Calculate the time difference
+    time_difference = get_time_difference(uptime, current_time)
+
     print(f"Time since last system reboot: {uptime}")
+    print(f"Current time: {current_time}")
+    print(f"Time difference: {time_difference}")
     
     # Save the uptime information to a JSON file
-    save_to_json(uptime)
+    save_to_json(uptime, current_time, time_difference)
 
 if __name__ == "__main__":
     main()
