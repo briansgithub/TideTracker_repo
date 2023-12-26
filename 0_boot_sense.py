cc6f8c0a041cbe41691b93a415f05f253c9759ae
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-26 00:19:16 -0600

    Start network manager before checking for an internet connection

--- 0_boot_sense.py
+++ 0_boot_sense.py
@@ -27,6 +27,9 @@ auto_run_wifi_script_path = os.path.join(os.path.dirname(os.path.realpath(__file
 plot_tides_script_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '1_pull_json_and_plot.py')
 no_wifi_errors_script_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'no_wifi_paste_over.py')
 
+command = "sudo systemctl start NetworkManager"
+subprocess.run(command, shell=True, check=True)
+
 try:
     pin_state = GPIO.input(gpio_pin)
     print(f"\n\nGPIO Pin BCM# {gpio_pin} is {pin_state}\n")
@@ -38,12 +41,21 @@ try:
         time.sleep(15) # wait some time for the system to initialize/stabilize
         subprocess.run(['sudo', 'bash', auto_run_wifi_script_path], check=True)
     else:
-        time.sleep(15)
+        time.sleep(5)
         if netman.have_active_internet_connection():
             print(f"--------- \nRunning the tides script located at:\n\t{plot_tides_script_path} ---------")
             subprocess.run(['sudo', 'python3', plot_tides_script_path], check=True)
         else: 
             subprocess.run(['sudo', 'python3', no_wifi_errors_script_path], check=True)
+
+except GPIOError as e:
+    print(f"GPIO error: {e}")
+except subprocess.CalledProcessError as e:
+    print(f"Error running subprocess: {e}")
+except netman.InternetConnectionError as e:
+    print(f"Internet connection error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
 finally:
     # Cleanup GPIO settings
