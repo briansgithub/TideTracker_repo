d8e634eddab0da2b598c7136f9c2188c3e3d9a0f
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-26 06:07:46 -0600

    Attempt fixing jank text by resaving .bmp

--- no_wifi_paste_over.py
+++ no_wifi_paste_over.py
@@ -15,24 +15,23 @@ from PIL import Image
 logging.info("epd7in5_V2 Paste over")
 epd = epd7in5_V2.EPD()
 epd.init()
-epd.Clear()
 
 logging.info("Paste error message over screen window")
 plot_image = Image.open(os.path.join(maindir, 'plot_image.bmp'))
 plot_image = plot_image.transpose(Image.ROTATE_180)
 
-err_img = Image.open(os.path.join(maindir, 'no_wifi.bmp'))
-err_img = err_img.transpose(Image.ROTATE_180)
+error_image = Image.open(os.path.join(maindir, 'no_wifi2.bmp'))
+error_image = error_image.transpose(Image.ROTATE_180)
+
 
 # Draw image in center of screen
-err_width, err_height = err_img.size
+err_width, err_height = error_image.size
 plot_width, plot_height = plot_image.size
 # Calculate the coordinates for the top-left corner to paste in the center
 paste_x = int((plot_width - err_width) / 2)
 paste_y = int((plot_height - err_height) / 2)
 
-
-plot_image.paste(err_img, (paste_x, paste_y))
+plot_image.paste(error_image, (paste_x, paste_y))
 
 epd.display(epd.getbuffer(plot_image))
 
