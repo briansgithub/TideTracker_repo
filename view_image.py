db8a672aa556fbf0576bb5cb16c0d8bb84b3b179
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-26 02:39:14 -0600

    Fix missing library in view_image.py

--- view_image.py
+++ view_image.py
@@ -5,6 +5,12 @@ import sys
 import os
 from PIL import Image
 import logging
+
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'e-ink_lib')
+maindir = os.path.dirname(os.path.realpath(__file__))
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
 from waveshare_epd import epd7in5_V2
 
 def main(image_path):
