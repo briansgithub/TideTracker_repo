d9ce7d83b47149dd44adcc2a77b13c479daf973c
Brian Ellsworth <physicsstudent137@gmail.com>
2023-12-26 07:01:08 -0600

    Fix jank text by converting both images to RGB so that there is no alpha or transparency channels. BMP is often pallet-based mode; convert to force  RGB mode

--- deleteme-temp.py
+++ deleteme-temp.py
@@ -3,38 +3,27 @@
 
 import sys
 import os
-libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'e-ink_lib')
-maindir = os.path.dirname(os.path.realpath(__file__))
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
 import logging
-from waveshare_epd import epd7in5_V2
 from PIL import Image
 
+maindir = os.path.dirname(os.path.realpath(__file__))
+
 logging.info("epd7in5_V2 Paste over")
-epd = epd7in5_V2.EPD()
-epd.init()
 
 logging.info("Paste error message over screen window")
-plot_image = Image.open(os.path.join(maindir, 'plot_image.bmp')).convert("RGB")
+plot_image = Image.open(os.path.join(maindir, 'error_image.bmp')).convert("RGB")
 plot_image = plot_image.transpose(Image.ROTATE_180)
 
 error_image = Image.open(os.path.join(maindir, 'no_wifi.bmp')).convert("RGB")
 error_image = error_image.transpose(Image.ROTATE_180)
 
-
-# Draw image in center of screen
+# Draw image in the center of the screen
 err_width, err_height = error_image.size
 plot_width, plot_height = plot_image.size
+
 # Calculate the coordinates for the top-left corner to paste in the center
 paste_x = int((plot_width - err_width) / 2)
 paste_y = int((plot_height - err_height) / 2)
 
-
 plot_image.paste(error_image, (paste_x, paste_y))
-
-epd.display(epd.getbuffer(plot_image))
-
-logging.info("EPD Go to Sleep...")
-epd.sleep() 
+plot_image.show()
