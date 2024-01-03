e7ff89f4cce2b2a655ee04c8100f4c9195c4bf4c
Brian Ellsworth <physicsstudent137@gmail.com>
2024-01-02 18:43:21 -0600

    Update script to run on boot to use /usr/bin/python3 not just "python3"

--- script_to_run_on_boot.sh
+++ script_to_run_on_boot.sh
@@ -2,5 +2,5 @@
 
 
 echo "Running script_to_run_on_boot.sh"
-python3 /home/pi/TideTracker_repo/0_boot_sense.py
+/usr/bin/python3 /home/pi/TideTracker_repo/0_boot_sense.py
 echo "Completed running script_to_run_on_boot.sh"
