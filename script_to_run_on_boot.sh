35c4bc0de627e2f36307ff67bd6893be85f3edcd
briansgithub <physicsstudent137@gmail.com>
2024-03-18 23:51:34 -0400

    Add command in script_to_run_on_boot.sh to kill PIDs associated with previous, broken wifi portal sessions

--- script_to_run_on_boot.sh
+++ script_to_run_on_boot.sh
@@ -2,5 +2,7 @@
 
 
 echo "Running script_to_run_on_boot.sh"
+ps aux | grep -i 'forked_wifi-connect-headless-rpi' | grep -v grep | awk '{print $2}' | xargs sudo kill
+sleep 1
 /usr/bin/python3 /home/pi/TideTracker_repo/0_boot_sense.py
 echo "Completed running script_to_run_on_boot.sh"
